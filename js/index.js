c3ccf04c3f29289cf8a7fa7277263c3c3cfdc46a
ikuto <ikuto0608@me.com>
2017-02-08 15:38:00 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$(function(){
+  var navMoblie = $("#nav-mobile");
+  navMoblie.on("click", "a", null, function () {
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    if (target.selector == '#trait') {
+      position = position - 30;
+    }
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+
+    document.getElementById("sidenav-overlay").click();
+  });
+});
+
+$(function(){
+  var navWeb = $("#nav-web");
+  navWeb.on("click", "a", null, function () {
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    if (target.selector == '#trait') {
+      position = position - 30;
+    }
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+  });
+});
+
+$(document).ready(function() {
+  var targetRed = $(".material_red").offset().top;
+  var targetYellow = $(".material_yellow").offset().top;
+  var targetBlue = $(".material_blue").offset().top;
+
+  var $w = $(window).scroll(function(){
+    if (targetYellow > $w.scrollTop()) {
+      $('#navigation').css({"background-color": "#F8BBD0"});
+    } else if (targetBlue > $w.scrollTop()) {
+      $('#navigation').css({"background-color": "#FFF9C4"});
+    } else {
+      $('#navigation').css({"background-color": "#BBDEFB"});
+    }
+  });
+});
